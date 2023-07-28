23690b0ff2767ed9dfeb111fc9a38593323b281a
mubashshir <abduganiyevmubashshir3767@gmail.com>
2023-07-28 15:30:39 +0500

    userda bazi o'zgarishlar boldi

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -24,7 +24,7 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    public ApiResponse registerUser(@ModelAttribute @Validated UserRegisterDto userRegisterDto) {
+    public ApiResponse registerUser(@ModelAttribute @Valid UserRegisterDto userRegisterDto) {
         return userService.create(userRegisterDto);
     }
 
